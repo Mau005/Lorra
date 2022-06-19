f1ff4b2b64b2e8b587d5c31c046690bcb0103c0e
mau <mpino1701@gmail.com>
2022-06-19 09:58:11 -0400

    se agrega informacion del personaje

--- Entidad/Persona.py
+++ Entidad/Persona.py
@@ -6,10 +6,27 @@ from Sprites.Animacion import Animacion
 
 class Persona(Entidad):
 
-    def __init__(self, sprite, rango, pos, size):
+    def __init__(self, manejadorsprite, nombre, email, vocacion, experiencia,
+                 sexo, hp_max, hp, mana_max, mana, puntos, vestimenta, posicion,
+                 acceso, pos, size):
         super().__init__(pos, size)
-        self.animacion = Animacion(sprite)
-        self.direccion = "sur"
+        self.manejadorsprite = manejadorsprite
+
+        self.nombre = nombre
+        self.email = email
+        self.vocacion = vocacion
+        self.experiencia = experiencia
+        self.sexo = sexo
+        self.hp_max = hp_max
+        self.hp = hp
+        self.mana_max = mana_max
+        self.mana = mana
+        self.puntos = puntos
+        self.vestimenta = vestimenta
+        self.posicion = posicion
+        self.acceso = acceso
+        self.animacion = Animacion(self.manejadorsprite.get_vestimenta(self.vestimenta))
+        self.direccion = "derecha"
         self.enMovimiento = True
         self.bg = Rectangle(source=self.animacion.get_sprite(), pos=pos, size=size)
 
@@ -18,7 +35,6 @@ class Persona(Entidad):
 
     def dibujar(self, canvas, *dt):
         self.animacion.set_direccion(self.direccion)
-        self.animacion.dibujar(self.enMovimiento,dt)
+        self.animacion.dibujar(self.enMovimiento, dt)
         self.bg.source = self.animacion.get_sprite()
         canvas.add(self.bg)
-
