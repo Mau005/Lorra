a6700851ecfaada571417ff5a6b34b5f1b484c64
mau <mpino1701@gmail.com>
2022-06-19 09:59:01 -0400

    se configura prueba bd para el jugador

--- Server.py
+++ Server.py
@@ -1,19 +1,27 @@
 import socket
 from core.Constantes import *
 from Networks.Hilo_Servidor import Hilo_Servidor
+from BaseDatos.Querys import Querys
 
 class Server:
 
     def __init__(self):
-        self.socket = socket.socket((IP, PORT))
-        self.socket.bind((5))
+        self.__configurarServidor()
+
+        self.querys = Querys()
+
+    def __configurarServidor(self):
+        self.socket = socket.socket()
+        self.socket.bind((IP, PORT))
+        self.socket.listen(5)
         self.enFuncionamiento = True
         self.lista_con = []
 
     def iniciar(self):
+        print("[OK] Servidor Online")
         while self.enFuncionamiento:
             clt, addr = self.socket.accept()
-            objeto = Hilo_Servidor(clt,addr)
+            objeto = Hilo_Servidor(clt, addr)
             self.lista_con.append(objeto)
             objeto.start()
 
