1188ddf5b96d6b6c32335a2146c24d7a070a34a3
administrador <mpino1701@gmail.com>
2022-06-22 16:34:42 -0400

    se agrega render

--- Ventanas/Juego.py
+++ Ventanas/Juego.py
@@ -4,48 +4,56 @@ from Entidad.Persona import Persona
 from Sprites.ManejadorSprites import ManejadorSprites
 from kivy.core.window import Window
 from Controles.Teclado import Teclado
-from core.Constantes import CANTIDAD_ESPACIOS_X ,CANTIDAD_ESPACIOS_Y
+from core.Constantes import CANTIDAD_ESPACIOS_X, CANTIDAD_ESPACIOS_Y
 from core.ControladorObjetos import ControladorObjetos
-from Objetos.Objetos import Objetos
+from Objetos.Suelos import Suelos
+from core.Render import Render
+
 
 class Juego(MDScreen):
     def __init__(self, **kargs):
-        super().__init__(size = Window.size, **kargs)
+        super().__init__(size=Window.size, **kargs)
         self.__configuracion_teclado()
         self.__actualizar_defecto()
         self.__configuracion_ventana()
         self.__contenido_canvas()
 
-
     def __configuracion_teclado(self):
         self._teclado = Window.request_keyboard(self._cerrar_teclado, self, 'text')
         self._teclado.bind(on_key_down=self._precionar_tecla)
-        self._teclado.bind(on_key_up = self._soltar_tecla)
+        self._teclado.bind(on_key_up=self._soltar_tecla)
         self.teclado = Teclado()
 
     def __actualizar_defecto(self):
         self.tamanio_cuadro = [Window.size[0] / CANTIDAD_ESPACIOS_X, Window.size[1] / CANTIDAD_ESPACIOS_Y]
-        self.pos_defecto = [self.tamanio_cuadro[0] * int(CANTIDAD_ESPACIOS_X / 2) - int(self.tamanio_cuadro[0]/2) ,
+        self.pos_defecto = [self.tamanio_cuadro[0] * int(CANTIDAD_ESPACIOS_X / 2) - int(self.tamanio_cuadro[0] / 2),
                             self.tamanio_cuadro[1] * int(CANTIDAD_ESPACIOS_Y / 2)]
 
     def __configuracion_ventana(self):
         self.name = "Juego"
-        self.maya = MDFloatLayout(size = self.size, size_hint = self.size_hint)
+        self.maya = MDFloatLayout(size=self.size, size_hint=self.size_hint)
         self.add_widget(self.maya)
         self.manejadorSprites = ManejadorSprites()
         self.controladorObjetos = ControladorObjetos()
+        self.jugador = Persona(self.manejadorSprites, 'Mau', 'prueba123', 1, 1, 1, 100, 100, 20, 20, 0, 1, '100:100:1',
+                               1, self.pos_defecto, self.tamanio_cuadro)
+
+        self.render = Render(self.jugador, self.teclado, self.tamanio_cuadro)
 
     def __contenido_canvas(self):
         self.dibujado_objetos = []
-        self.jugador = Persona(self.manejadorSprites, 'Mau', 'prueba123', 1, 1, 1, 100, 100, 20, 20, 0, 1, '100:100:1', 1, self.pos_defecto, self.tamanio_cuadro)
-        self.objeto1 = Objetos("suelo", self.manejadorSprites.get_objetos(1),[0,0],self.tamanio_cuadro)
-        self.dibujado_objetos.append(Objetos("suelo", self.manejadorSprites.get_objetos(1),[0,0],self.tamanio_cuadro))
-        self.dibujado_objetos.append(Objetos("suelo", self.manejadorSprites.get_objetos(2),[self.tamanio_cuadro[0]*1,0],self.tamanio_cuadro))
-        self.dibujado_objetos.append(Objetos("suelo", self.manejadorSprites.get_objetos(3),[self.tamanio_cuadro[0]*2,0],self.tamanio_cuadro))
-        self.dibujado_objetos.append(Objetos("suelo", self.manejadorSprites.get_objetos(4),[self.tamanio_cuadro[0]*3,0],self.tamanio_cuadro))
-        self.dibujado_objetos.append(
-            Objetos("suelo", self.manejadorSprites.get_objetos(5), [self.tamanio_cuadro[0] * 5, self.tamanio_cuadro[1] * 5],
-                    self.tamanio_cuadro))
+        objtest = {"solido": False, "tipo": "Suelo", "accion": 0}
+        self.render.registrar(
+            Suelos(objtest, "suelo", self.manejadorSprites.get_objetos(1), [0, 0], self.tamanio_cuadro))
+        self.render.registrar(
+            Suelos(objtest, "suelo", self.manejadorSprites.get_objetos(2), [0, self.tamanio_cuadro[1] * 1],
+                   self.tamanio_cuadro))
+        self.render.registrar(
+            Suelos(objtest, "suelo", self.manejadorSprites.get_objetos(3), [0, self.tamanio_cuadro[1] * 2],
+                   self.tamanio_cuadro))
+        self.render.registrar(
+            Suelos(objtest, "suelo", self.manejadorSprites.get_objetos(5), [0, self.tamanio_cuadro[1] * 3],
+                   self.tamanio_cuadro))
 
     def _cerrar_teclado(self, *args):
         print("Se ha cerrado el teclado")
@@ -60,7 +68,6 @@ class Juego(MDScreen):
 
         self.teclado.soltar_tecla(contenido)
 
-
     def _precionar_tecla(self, *args):
         # Objetivo de 2 capturas de teclas
         # [0] posicion es espacio en memoria
@@ -69,31 +76,14 @@ class Juego(MDScreen):
         # [3] Tecla especial+ tecla
         self.teclado.precionar_tecla(args[1][0])
 
-    def movimiento_jugador(self, mover, dt):
-        velocidad = 85 * dt[0]
-
-        if self.teclado.get_tecla(119):
-            self.jugador.caminar(mover, "norte", dt)
-        elif self.teclado.get_tecla(115):
-            self.jugador.caminar(mover, "sur", dt)
-        elif self.teclado.get_tecla(97):
-            self.jugador.caminar(mover, "izquierda", dt)
-        elif self.teclado.get_tecla(100):
-            self.jugador.caminar(mover, "derecha", dt)
-
-
     def actualizar(self, *dt):
         self.__actualizar_defecto()
         self.jugador.actualizar(dt)
         self.jugador.pos = self.pos_defecto
-        self.objeto1.actualizar(dt)
-        for elementos in self.dibujado_objetos:
-            self.movimiento_jugador(elementos, dt)
-            elementos.actualizar(dt)
+        self.render.actualizar(self.tamanio_cuadro, dt)
 
     def dibujar(self, *dt):
         self.size = Window.size
         self.maya.canvas.clear()
-        for elementos in self.dibujado_objetos:
-            elementos.dibujar(self.maya.canvas, self.tamanio_cuadro, dt)
-        self.jugador.dibujar(self.maya.canvas, dt)
+        self.render.dibujar(self.maya.canvas, dt)
+        self.jugador.dibujar(self.maya.canvas, self.tamanio_cuadro, dt)
