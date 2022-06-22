e8b240866f7d6129434051f42ded07998ee1419b
administrador <mpino1701@gmail.com>
2022-06-22 10:55:46 -0400

    se agrega metodo caminar y velocidad al jugador

--- Entidad/Persona.py
+++ Entidad/Persona.py
@@ -16,6 +16,7 @@ class Persona(Entidad):
         self.email = email
         self.vocacion = vocacion
         self.experiencia = experiencia
+        self.velocidad_inicial = 80
         self.sexo = sexo
         self.hp_max = hp_max
         self.hp = hp
@@ -30,12 +31,23 @@ class Persona(Entidad):
         self.enMovimiento = False
         self.bg = Rectangle(source=self.animacion.get_sprite(), pos=pos, size=size)
 
+    def caminar(self, mover, direccion, dt):
+        velocidad = self.velocidad_inicial * dt[0]
+        self.direccion = direccion
+        self.enMovimiento = True
+        if direccion == "norte":
+            mover.pos[1] -= velocidad
+        elif direccion == "sur":
+            mover.pos[1] += velocidad
+        elif direccion == "izquierda":
+            mover.pos[0] += velocidad
+        elif direccion == "derecha":
+            mover.pos[0] -= velocidad
+
     def actualizar(self, teclado,*dt):
-        pass
+        self.bg.pos = self.pos
 
     def dibujar(self, canvas, *dt):
-        self.animacion.set_direccion(self.direccion)
-        self.animacion.dibujar(self.enMovimiento, dt)
+        self.animacion.dibujar(self.enMovimiento, dt, direccion=self.direccion)
         self.bg.source = self.animacion.get_sprite()
-        self.bg.pos = self.pos
         canvas.add(self.bg)
