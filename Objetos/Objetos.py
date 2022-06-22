b4b49d3a57676afdeee2696d6e67eb5d0ef063da
administrador <mpino1701@gmail.com>
2022-06-22 16:34:11 -0400

    se mejora sintaxis

--- Objetos/Objetos.py
+++ Objetos/Objetos.py
@@ -2,6 +2,7 @@ from Sprites.Animacion import Animacion
 from kivy.graphics import Rectangle
 from Entidad.Entidad import Entidad
 
+
 class Objetos(Entidad):
 
     def __init__(self, nombre, sprite, pos, size):
@@ -11,19 +12,14 @@ class Objetos(Entidad):
         self.animacion = Animacion(sprite)
         if len(self.sprite.get("sur")) >= 2:
             self.enMovimiento = True
-            self.bg = Rectangle(source=self.animacion.ruta_predefinida, pos = pos, size = size)
+            self.bg = Rectangle(source=self.animacion.ruta_predefinida, pos=pos, size=size)
         else:
             self.enMovimiento = False
-            self.bg = Rectangle(source=self.animacion.ruta_sprite, pos = pos, size = size)
-
-
-
-
+            self.bg = Rectangle(source=self.animacion.ruta_sprite, pos=pos, size=size)
 
     def actualizar(self, dt):
         self.animacion.actualizar(dt)
         self.bg.pos = self.pos
-
 
     def dibujar(self, canvas, size, dt):
         self.bg.size = size
