3c89810e8a7f86a99096f675e46c21c7e0c0d104
administrador <mpino1701@gmail.com>
2022-06-22 02:30:35 -0400

    agregando clases para mapa y objetos

---
+++ Objetos/Objetos.py
@@ -0,0 +1,13 @@
+from abc import abstractmethod
+
+class Objectos:
+
+    def __init__(self, nombre, sprite):
+        pass
+
+    def actualizar(self, dt):
+        pass
+
+
+    def dibujar(self, dt):
+        pass
