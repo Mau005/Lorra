adf70fceaeab24261ffbab4350fe5d55c4dffc93
mau <mpino1701@gmail.com>
2022-06-28 22:52:36 -0400

    se agrega func para recojer objetos

--- core/ControladorObjetos.py
+++ core/ControladorObjetos.py
@@ -1,22 +1,28 @@
 from core.Herramientas import Herramientas
+from Objetos.Suelos import Suelos
 
 class ControladorObjetos:
 
-    def __init__(self, path=None):
+    def __init__(self, manejadorSprites, path=None):
+        self.manejadorSprites = manejadorSprites
         if path is None:
             self.__datos = Herramientas.cargar_json("Data/Objetos/objetos.json")
         else:
             self.__datos = Herramientas.cargar_json(path)
 
-
-    def get_objeto(self, idObjeto):
+    def get_objeto(self, idObjeto, pos , size):
         """
         Devolvera los datos ingresados desde el objeto json
         se convierte a str para poder obtener la informacion correpsondiente
         :param idObjeto:
         :return:
         """
-        return self.__datos[str(idObjeto)]
+        datos = self.__datos[str(idObjeto)]
+
+        if datos.get("tipo") == "Suelo":
+            return Suelos(datos,self.manejadorSprites.get_objetos(int(idObjeto)), pos, size)
+
+        return None
 
     def get_all(self):
         return self.__datos
